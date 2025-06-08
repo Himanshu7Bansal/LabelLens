2b335a119c7ff5a179c15203c7db858d2778ff93
Himanshu Bansal <himanshu07bansal@gmail.com>
2025-06-08 23:22:26 +0530

    fix clipped UI

--- app/src/main/java/dev/h7b/labelLens/ui/screen/objectIdentifier/ObjectIdentifierScreen.kt
+++ app/src/main/java/dev/h7b/labelLens/ui/screen/objectIdentifier/ObjectIdentifierScreen.kt
@@ -2,9 +2,9 @@ package dev.h7b.labelLens.ui.screen.objectIdentifier
 
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,7 +13,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,10 +22,9 @@ import dev.h7b.labelLens.ui.components.ButtonPrimary
 import dev.h7b.labelLens.ui.components.Image
 import dev.h7b.labelLens.ui.components.ImageWithLabel
 import dev.h7b.labelLens.ui.theme.HorizontalPadding
-import dev.h7b.labelLens.ui.theme.VerticalPadding
 
 @Composable
-fun BoxScope.ObjectIdentifierScreen(
+fun ObjectIdentifierScreen(
     uri: Uri,
     bitmap: Bitmap?,
     detectedObjectBoxes: SnapshotStateList<DetectedObjectBox>,
@@ -35,25 +33,29 @@ fun BoxScope.ObjectIdentifierScreen(
     val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
-            .padding(horizontal = HorizontalPadding, vertical = VerticalPadding)
-            .fillMaxWidth()
-            .verticalScroll(scrollState)
+            .fillMaxSize()
+            .padding(horizontal = HorizontalPadding)
     ) {
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(text = stringResource(R.string.original_image))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+                .verticalScroll(scrollState)
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(text = stringResource(R.string.original_image))
+            Spacer(modifier = Modifier.height(12.dp))
+            Image(uri = uri)
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(text = stringResource(R.string.object_detection_image))
+            Spacer(modifier = Modifier.height(12.dp))
+            ImageWithLabel(bitmap = bitmap, detectedObjectBoxes = detectedObjectBoxes)
+        }
         Spacer(modifier = Modifier.height(12.dp))
-        Image(uri = uri)
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(text = stringResource(R.string.object_detection_image))
-        Spacer(modifier = Modifier.height(12.dp))
-        ImageWithLabel(bitmap = bitmap, detectedObjectBoxes = detectedObjectBoxes)
+        ButtonPrimary(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.go_to_main),
+            onClick = onGoToMain
+        )
     }
-    ButtonPrimary(
-        modifier = Modifier
-            .padding(start = HorizontalPadding, end = HorizontalPadding, bottom = VerticalPadding)
-            .fillMaxWidth()
-            .align(Alignment.BottomCenter),
-        text = stringResource(R.string.go_to_main),
-        onClick = onGoToMain
-    )
 }
